e785139f4bf20a08f3471d80f835f4512761df7f
IlDivinatore01 <simo.m2001@gmail.com>
2024-05-14 13:21:10 +0200

    Add MongoDB

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 // Definizione dell'URL del database MongoDB
-const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network-for-music';
+const dbURI = process.env.MONGODB_URI || 'mongodb+srv://simone:<password>@cluster0.7dpoap2.mongodb.net/';
 
 // Esportazione dell'URL del database
 module.exports = {
